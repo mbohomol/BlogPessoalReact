887341adf9f04b6e88a015cf77d38de661ea50f1
Milene Bohomol <mbohomol@gmail.com>
2022-06-01 19:43:52 -0300

    Blog Pessoal React - BackEnd + FrontEnd

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Typography, Box, Grid, Button} from '@material-ui/core';
+import TabPostagem from '../../components/postagens/tabpostagem/TabPostagem';
 import './Home.css';
 
 function Home() {
@@ -21,6 +22,7 @@ function Home() {
                 <img src={require('../../assets/images/sorvetes1.jpg')}  alt="" width="700px" height="500px"/>
                 </Grid>
                 <Grid xs={12} style={{ backgroundColor: "white" }}>
+                <TabPostagem />
                 </Grid>
             </Grid>
         </>
