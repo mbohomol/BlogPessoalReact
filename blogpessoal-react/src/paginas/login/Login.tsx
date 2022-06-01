4419ac3b352e1d062b4757dc33b8998184b23311
Milene Bohomol <mbohomol@gmail.com>
2022-05-31 22:09:25 -0300

    Blog Pessoal - BackEnd + FrontEnd

--- blogpessoal-react/src/paginas/login/Login.tsx
+++ blogpessoal-react/src/paginas/login/Login.tsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect, ChangeEvent } from 'react';
 import { Grid, Box, Typography, TextField, Button } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import useLocalStorage from 'react-use-localstorage';
+import { login } from '../../services/Service';
+import UserLogin from '../../models/UserLogin';
 import './Login.css';
 
 function Login() {
+    let navigate = useNavigate();
+    const [token, setToken] = useLocalStorage('token');
+    const [userLogin, setUserLogin] = useState<UserLogin>(
+        {
+            id: 0,
+            usuario: '',
+            senha: '',
+            token: ''
+        }
+        )
+
+        function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+
+            setUserLogin({
+                ...userLogin,
+                [e.target.name]: e.target.value
+            })
+        }
+
+            useEffect(()=>{
+                if(token != ''){
+                    navigate('/home')
+                }
+            }, [token])
+
+        async function onSubmit(e: ChangeEvent<HTMLFormElement>){
+            e.preventDefault();
+            try{
+                await login(`/usuarios/logar`, userLogin, setToken)
+            
+
+                alert('Usuário logado com sucesso!');
+            }catch(error){
+                alert('Dados do usuário inconsistentes. Erro ao logar!');
+            }
+        }
 
     return (
         <Grid container direction='row' justifyContent='center' alignItems='center'>
@@ -25,7 +64,9 @@ function Login() {
                         <Box marginRight={1}>
                             <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
                         </Box>
+                        <Link to='/cadastrousuario'>
                             <Typography variant='subtitle1' gutterBottom align='center' style={{fontWeight: 'bold'}}>Cadastre-se</Typography>
+                            </Link>
                     </Box>
                 </Box>
             </Grid>
